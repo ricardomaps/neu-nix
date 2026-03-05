6a5537f2e32f83531ad4df28d2269dc3294c07cc
ricmaps <ricardomapurungajunior@gmail.com>
2026-03-04 23:55:45 -0300

    added hevel and neumenu to packages scope

--- packages/default.nix
+++ packages/default.nix
@@ -3,6 +3,8 @@ let
   scope = pkgs.lib.makeScope pkgs.newScope (self: {
     neuwld      = self.callPackage ./neuwld    { };
     neuswc      = self.callPackage ./neuswc    { };
+    neumenu     = self.callPackage ./neumenu   { };
+    hevel       = self.callPackage ./hevel     { };
     shko        = self.callPackage ./shko      { };
   });
 in
